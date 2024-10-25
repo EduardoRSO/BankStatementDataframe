1ad304a9cb3677fd465ccd240c9137e57f6c3c2b
EduardoRSO <edusillva784@gmail.com>
2024-10-25 09:00:59 -0300

    fix: updated imports in parser.py to include bank_statement_parser path for correct module referencing

--- bank_statement_parser/formats/bradesco.py
+++ bank_statement_parser/formats/bradesco.py
@@ -1,11 +1,11 @@
-from formats.parser import Parser
+from bank_statement_parser.formats.parser import Parser
 
 class BradescoParser(Parser):
 
     def __init__(self, file_path, password_list=None):
         # Chama o construtor da classe pai (Parser) para inicializar os atributos
         super().__init__(file_path, password_list)
-            
+
     def extract_data(self):
         # Implementação específica para extrair dados do extrato do Bradesco
         print("Extraindo dados do extrato Bradesco")
--- bank_statement_parser/formats/itau.py
+++ bank_statement_parser/formats/itau.py
@@ -1,10 +1,10 @@
-from formats.parser import Parser
+from bank_statement_parser.formats.parser import Parser
 
 class ItauParser(Parser):
     def __init__(self, file_path, password_list=None):
         # Chama o construtor da classe pai (Parser) para inicializar os atributos
         super().__init__(file_path, password_list)
-        
+
     def extract_data(self):
         # Implementação específica para extrair dados do extrato Itaú
         print("Extraindo dados do extrato Itaú")
