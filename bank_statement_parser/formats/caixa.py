bc14cb1a5a4f4e5ee8e044316604f883c8d34bd8
EduardoRSO <edusillva784@gmail.com>
2024-10-25 08:28:30 -0300

    refactor: initialized parent class Parser in all specific parsers (Bradesco, Caixa, Carrefour, Itau) to ensure consistent attribute setup

--- bank_statement_parser/formats/caixa.py
+++ bank_statement_parser/formats/caixa.py
@@ -1,6 +1,11 @@
 from formats.parser import Parser
 
 class CaixaParser(Parser):
+
+    def __init__(self, file_path, password_list=None):
+        # Chama o construtor da classe pai (Parser) para inicializar os atributos
+        super().__init__(file_path, password_list)
+            
     def extract_data(self):
         # Implementação específica para extrair dados do extrato da Caixa
         print("Extraindo dados do extrato Caixa")
--- bank_statement_parser/formats/carrefour.py
+++ bank_statement_parser/formats/carrefour.py
@@ -1,6 +1,11 @@
 from formats.parser import Parser
 
 class CarrefourParser(Parser):
+
+    def __init__(self, file_path, password_list=None):
+        # Chama o construtor da classe pai (Parser) para inicializar os atributos
+        super().__init__(file_path, password_list)
+        
     def extract_data(self):
         # Implementação específica para extrair dados do extrato Carrefour
         print("Extraindo dados do extrato Carrefour")
