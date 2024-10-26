1e06170d105cc2146befee08b3a4d0f7b66e979d
EduardoRSO <edusillva784@gmail.com>
2024-10-26 11:13:32 -0300

    chore: changed testing in parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,17 +1,13 @@
 import os
 import unittest
-from PyPDF2 import PdfWriter, PdfReader
 from bank_statement_parser.formats.parser_factory import ParserFactory, ItauParser, CaixaParser, BradescoParser, CarrefourParser
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4
 
 class TestParserFactory(unittest.TestCase):
 
     def setUp(self):
         """
-        Cria arquivos PDF protegidos por senha para cada tipo de parser.
+        Configura arquivos PDF para cada tipo de parser com senhas específicas.
         """
-        self.test_text = "Texto de teste para extração"
         self.files = {
             "caixa_extrato.pdf": "senha_caixa",
             "bradesco_extrato.pdf": "senha_bradesco",
@@ -19,27 +15,9 @@ class TestParserFactory(unittest.TestCase):
             "itau_extrato.pdf": "senha_itau"
         }
 
-        # Cria cada arquivo PDF com o texto e a senha de teste
-        for file_name, password in self.files.items():
-            # Gera o PDF com texto usando reportlab
-            c = canvas.Canvas(file_name, pagesize=A4)
-            c.drawString(100, 750, self.test_text)
-            c.save()
-
-            # Lê o PDF gerado e adiciona ao PdfWriter para criptografar
-            writer = PdfWriter()
-            with open(file_name, "rb") as f:
-                reader = PdfReader(f)
-                writer.add_page(reader.pages[0])
-                writer.encrypt(password)
-
-            # Salva o PDF criptografado
-            with open(file_name, "wb") as f:
-                writer.write(f)
-
-    def test_password_protected_pdfs(self):
+    def test_parser_selection(self):
         """
-        Testa se o ParserFactory retorna a instância correta e extrai o texto esperado de cada PDF protegido.
+        Testa se o ParserFactory retorna a instância correta do parser com base no nome do arquivo.
         """
         for file_name, password in self.files.items():
             parser = ParserFactory.get_parser(file_name, password_list=[password])
@@ -50,17 +28,8 @@ class TestParserFactory(unittest.TestCase):
                 "carrefour_extrato.pdf": CarrefourParser,
                 "itau_extrato.pdf": ItauParser
             }[file_name]
-            self.assertIsInstance(parser, expected_parser_class)
-            self.assertEqual(parser.text.replace('\n',''), self.test_text, f"Texto extraído do {file_name} não corresponde ao texto esperado.")
 
-    def tearDown(self):
-        """
-        Remove os arquivos PDF criados para os testes.
-        """
-        for file_name in self.files:
-            if os.path.exists(file_name):
-                # os.remove(file_name)
-                pass
+            self.assertIsInstance(parser, expected_parser_class, f"O parser para {file_name} não é da classe esperada.")
 
 if __name__ == "__main__":
     unittest.main()
