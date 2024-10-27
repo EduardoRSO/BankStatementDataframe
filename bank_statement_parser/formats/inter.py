bddf6b72b7a811034318513ce1763123d1f116a0
EduardoRSO <edusillva784@gmail.com>
2024-10-27 13:27:54 -0300

    Refactored InterParser to enhance transaction classification and improve dashboard accuracy.
    Changes:
    - Added logic to determine transaction type (`Receitas` or `Custos`) based on `valor_transacao` values. This approach ensures that amounts are accurately categorized as income or expenses before attempting further classification.
    - Updated `classificar_categoria` method to check `tipo_hierarquia` first, then match keywords in `descricao_transacao` based on predefined categories. This hierarchical classification process prevents incorrect categorization due to inconsistent descriptions in Inter bank statements.
    - Modified `transform_to_dataframe` method to calculate `entrada` and `saida` based on the new `Receitas`/`Custos` logic, ensuring net values (`net`) accurately reflect transaction balances.

--- bank_statement_parser/formats/inter.py
+++ bank_statement_parser/formats/inter.py
@@ -12,24 +12,24 @@ class InterParser(Parser):
         'Salários e Rendimentos', 'Investimentos', 'Freelances e Serviços', 
         'Aluguéis Recebidos', 'Reembolsos e Reversões', 'Prêmios e Concursos', 'Outros Créditos'
     ]
-    SALARIOS_RENDIMENTOS = ['credito evento b3:', 'resgate:', 'aplicacao:']
-    INVESTIMENTOS = []
+    SALARIOS_RENDIMENTOS = ['pix recebido']
+    INVESTIMENTOS = ['credito evento b3:', 'aplicacao:', 'aplicacao em fundos:', 'cashback fundos:', 'recebimento de juros:', 'credito b3:', 'credito resgate fundo:', 'cashback:','credito tesouro direto:', 'pagamento / rec. juros:', 'cashback cartao de credito', 'vencimento renda fixa:', 'aplicacao transacao sem descricao', 'cred pontos meu porquinho:','aplicacao poupanca transacao sem descricao', 'dep conta intl loop:','cred pontos conta global:', 'deposito:','credito conta global de inv:']
     FREELANCES_SERVICOS = []
     ALUGUEIS_RECEBIDOS = []
-    REEMBOLSOS_REVERSOES = []
+    REEMBOLSOS_REVERSOES = ['estorno:']
     PREMIOS_CONCURSOS = []
-    OUTROS_CREDITOS = ['pix recebido']
+    OUTROS_CREDITOS = ['antecipacao de recebiveis - bovespa: "de banco inter s/a" r$ 266,51', 'antecipacao de recebiveis - bovespa: "de banco inter s/a" r$ 296,03', 'antecipacao de recebiveis - bovespa: "de banco inter s/a" r$ 3.972,93', 'antecipacao de recebiveis - bovespa: "de banco inter s/a" r$ 640,92,94']
     
-    MORADIA = ['pagamento -r$']
-    TRANSPORTE = []
+    MORADIA = ['pagamento -r$', 'pagamento efetuado', 'compra no debito:', 'boleto recebido:']
+    TRANSPORTE = ['shell box - abastecimento:']
     ALIMENTACAO = []
     EDUCACAO = []
     SAUDE_BEM_ESTAR = []
     LAZER_ENTRETENIMENTO = []
-    VESTUARIO_COMPRAS_PESSOAIS = []
-    IMPOSTOS_TAXAS = []
-    SERVICOS_ASSINATURAS = ['tim s a']
-    OUTROS_DEBITOS = []
+    VESTUARIO_COMPRAS_PESSOAIS = ['compra:', 'venda:', 'cred digital boleto:']
+    IMPOSTOS_TAXAS = ['imposto:', 'debito iof conta global de inv:', 'iof op conta intl loop:', 'iof:']
+    SERVICOS_ASSINATURAS = ['tim s a', 'recarga: "recarga tim"']
+    OUTROS_DEBITOS = ['resgate:', 'resgate de fundo:', 'saque:','pix enviado:','debito b3:','debito tesouro direto:','debito fundo:', 'credito liberado: "pix no credito"', 'resgate poupanca transacao sem descricao', 'debito oferta publica:', 'debito conta global de inv:', 'antecipacao de recebiveis - bovespa: "de banco inter s/a" r$ 850,94']
 
     def __init__(self, file_path, password_list=None):
         super().__init__(file_path, password_list)
@@ -48,18 +48,21 @@ class InterParser(Parser):
         curr_date = ''
         while curr_pos < len(splitted_lines):
             curr_line = splitted_lines[curr_pos]
-            date_match = re.search(r'(\d+ de [A-Za-z]+ de \d{4})', curr_line)
+            for remove in removes:
+                curr_line = re.sub(remove,'',curr_line)
+            date_match = re.search(r'(\d|\d{2}) de [A-Za-z]+ de \d{4}', curr_line)
             if date_match:
                 curr_date = date_match.group()
             transaction_match = re.search(self.PATTERN, curr_line)
             if transaction_match:
-                for remove in removes:
-                    curr_line = re.sub(remove,'',curr_line)
                 curr_line = curr_line.split(' ')
+                formated_date = datetime.strptime(curr_date, '%d de %B de %Y').strftime('%d/%m/%Y')
+                extracted_value = ''.join(curr_line[-4:-2]).lower().replace('r$','')
+                transaction_description = ' '.join(curr_line[0:-4])
                 tmp = {
-                    'data_transacao': datetime.strptime(curr_date, '%d de %B de %Y').strftime('%d/%m/%Y'),
-                    'valor_transacao': curr_line[-3],
-                    'descricao_transacao': ' '.join(curr_line[0:-2])
+                    'data_transacao': formated_date,
+                    'valor_transacao': extracted_value,
+                    'descricao_transacao': transaction_description
                 }
                 self.data.append(tmp)
             curr_pos +=1
@@ -72,51 +75,54 @@ class InterParser(Parser):
             'descricao_transacao': 'descricao_transacao'
         }, inplace=True)
         
-        df['valor_transacao'] = pd.to_numeric(df['valor_transacao'].str.replace('.', '').str.replace(',', '.').str.replace('-', '').str.strip())
-        df['categoria_transacao'] = df['descricao_transacao'].apply(self.classificar_categoria)
-        df['tipo_hierarquia'] = df['categoria_transacao'].apply(lambda x: 'Receitas' if x in self.RECEITAS_CATEGORIAS else 'Custos')
-        df['entrada'] = df.apply(lambda row: abs(row['valor_transacao']) if row['tipo_hierarquia'] == 'Receitas' else 0, axis=1)
-        df['saida'] = df.apply(lambda row: abs(row['valor_transacao']) if row['tipo_hierarquia'] == 'Custos' else 0, axis=1)
+        df['valor_transacao'] = pd.to_numeric(df['valor_transacao'].str.replace('.', '').str.replace(',', '.').str.strip())
+        df['tipo_hierarquia'] = df['valor_transacao'].apply(lambda x: 'Receitas' if x >= 0 else 'Custos')
+        df['categoria_transacao'] = df.apply(self.classificar_categoria,axis=1)
+        df['entrada'] = df['valor_transacao'].apply(lambda x: abs(x) if x >= 0 else 0)
+        df['saida'] = df['valor_transacao'].apply(lambda x: abs(x) if x < 0 else 0)
         df['net'] = df['entrada'] - df['saida']
         df['origem'] = 'Inter'
         self.transformed_data = df
 
-    def classificar_categoria(self, descricao):
-        descricao = descricao.lower()
-        
-        if any(word in descricao for word in self.SALARIOS_RENDIMENTOS):
-            return 'Salários e Rendimentos'
-        elif any(word in descricao for word in self.INVESTIMENTOS):
-            return 'Investimentos'
-        elif any(word in descricao for word in self.FREELANCES_SERVICOS):
-            return 'Freelances e Serviços'
-        elif any(word in descricao for word in self.ALUGUEIS_RECEBIDOS):
-            return 'Aluguéis Recebidos'
-        elif any(word in descricao for word in self.REEMBOLSOS_REVERSOES):
-            return 'Reembolsos e Reversões'
-        elif any(word in descricao for word in self.PREMIOS_CONCURSOS):
-            return 'Prêmios e Concursos'
-        elif any(word in descricao for word in self.OUTROS_CREDITOS):
-            return 'Outros Créditos'
-        elif any(word in descricao for word in self.MORADIA):
-            return 'Moradia'
-        elif any(word in descricao for word in self.TRANSPORTE):
-            return 'Transporte'
-        elif any(word in descricao for word in self.ALIMENTACAO):
-            return 'Alimentação'
-        elif any(word in descricao for word in self.EDUCACAO):
-            return 'Educação'
-        elif any(word in descricao for word in self.SAUDE_BEM_ESTAR):
-            return 'Saúde e Bem-estar'
-        elif any(word in descricao for word in self.LAZER_ENTRETENIMENTO):
-            return 'Lazer e Entretenimento'
-        elif any(word in descricao for word in self.VESTUARIO_COMPRAS_PESSOAIS):
-            return 'Vestuário e Compras Pessoais'
-        elif any(word in descricao for word in self.IMPOSTOS_TAXAS):
-            return 'Impostos e Taxas'
-        elif any(word in descricao for word in self.SERVICOS_ASSINATURAS):
-            return 'Serviços e Assinaturas'
-        elif any(word in descricao for word in self.OUTROS_DEBITOS):
-            return 'Outros Débitos'
-        else:
-            return 'Outros'
+    def classificar_categoria(self, row:pd.DataFrame):
+        descricao = row['descricao_transacao'].lower()
+        if row['tipo_hierarquia'] == 'Receitas':
+            if any(word in descricao for word in self.SALARIOS_RENDIMENTOS):
+                return 'Salários e Rendimentos'
+            elif any(word in descricao for word in self.INVESTIMENTOS):
+                return 'Investimentos'
+            elif any(word in descricao for word in self.FREELANCES_SERVICOS):
+                return 'Freelances e Serviços'
+            elif any(word in descricao for word in self.ALUGUEIS_RECEBIDOS):
+                return 'Aluguéis Recebidos'
+            elif any(word in descricao for word in self.REEMBOLSOS_REVERSOES):
+                return 'Reembolsos e Reversões'
+            elif any(word in descricao for word in self.PREMIOS_CONCURSOS):
+                return 'Prêmios e Concursos'
+            elif any(word in descricao for word in self.OUTROS_CREDITOS):
+                return 'Outros Créditos'
+            else:
+                return 'Outros'
+        if row['tipo_hierarquia'] == 'Custos':
+            if any(word in descricao for word in self.MORADIA):
+                return 'Moradia'
+            elif any(word in descricao for word in self.TRANSPORTE):
+                return 'Transporte'
+            elif any(word in descricao for word in self.ALIMENTACAO):
+                return 'Alimentação'
+            elif any(word in descricao for word in self.EDUCACAO):
+                return 'Educação'
+            elif any(word in descricao for word in self.SAUDE_BEM_ESTAR):
+                return 'Saúde e Bem-estar'
+            elif any(word in descricao for word in self.LAZER_ENTRETENIMENTO):
+                return 'Lazer e Entretenimento'
+            elif any(word in descricao for word in self.VESTUARIO_COMPRAS_PESSOAIS):
+                return 'Vestuário e Compras Pessoais'
+            elif any(word in descricao for word in self.IMPOSTOS_TAXAS):
+                return 'Impostos e Taxas'
+            elif any(word in descricao for word in self.SERVICOS_ASSINATURAS):
+                return 'Serviços e Assinaturas'
+            elif any(word in descricao for word in self.OUTROS_DEBITOS):
+                return 'Outros Débitos'
+            else:
+                return 'Outros'
