7ad9f0178f63c2cb98f7d95709c405d0cae61f8f
EduardoRSO <edusillva784@gmail.com>
2024-10-27 15:52:32 -0300

    feat: implemented 'transform_to_dataframe' method in the BradescoParser to solve text extraction problem, because what changes is the value color instead of '+' or '-' symbol

--- bank_statement_parser/formats/bradesco.py
+++ bank_statement_parser/formats/bradesco.py
@@ -11,7 +11,7 @@ class BradescoParser(Parser):
         if self.text != "":
             self.extract_data()
             if self.data != []:
-                self.transform_to_dataframe('Bradesco')
+                self.transform_to_dataframe()
                 if not self.transformed_data.empty:
                     self.save_transformed_dataframe(self.transformed_data)
 
@@ -34,13 +34,32 @@ class BradescoParser(Parser):
                         if date_match:
                             curr_date = date_match.group()
                             curr_line = curr_line.replace(curr_date, "")
-                        curr_line = curr_line.split(" ")
+                        splitted_line = curr_line.split(" ")
+                        extracted_date = curr_date
+                        extracted_value = splitted_line[-2]
+                        extracted_description = ' '.join(splitted_line[0:-2])
                         tmp = {
-                            'data_transacao': curr_date,
-                            'valor_transacao': curr_line[-2],
-                            'descricao_transacao': ' '.join(curr_line[0:-2])
+                            'data_transacao': extracted_date,
+                            'valor_transacao': extracted_value,
+                            'descricao_transacao': extracted_description
                         }
                         self.data.append(tmp)
                     curr_pos +=1
             else:
                 curr_pos +=1
+
+    def transform_to_dataframe(self):
+        df = pd.DataFrame(self.data)
+        df.rename(columns={
+            'data_transacao': 'data_transacao',
+            'valor_transacao': 'valor_transacao',
+            'descricao_transacao': 'descricao_transacao'
+        }, inplace=True)  
+        df['valor_transacao'] = pd.to_numeric(df['valor_transacao'].str.replace('.', '').str.replace(',', '.').str.strip())
+        df['categoria_transacao'] = df.apply(self.classificar_categoria, axis=1)
+        df['tipo_hierarquia'] = df['categoria_transacao'].apply(lambda x: 'Receitas' if x in self.receitas_definitions.keys() else 'Custos')
+        df['entrada'] = df['tipo_hierarquia'].apply(lambda x: abs(x) if x == 'Receitas' else 0)
+        df['saida'] = df['tipo_hierarquia'].apply(lambda x: abs(x) if x != 'Receitas'else 0)
+        df['net'] = df['entrada'] - df['saida']
+        df['origem'] = 'Bradesco'
+        self.transformed_data = df
